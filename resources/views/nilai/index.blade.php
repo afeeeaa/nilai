926fc297fa88f7954e0ee942e3d68a0b3f2d34b7
afeeeaa <nurulafifah23032003@gmail.com>
2024-01-29 16:13:28 +0700

    up 16:13

--- resources/views/nilai/index.blade.php
+++ resources/views/nilai/index.blade.php
@@ -91,7 +91,7 @@
             <td>{{ $item->nama }}</td>
             <td class="text-center">
                 <a href="{{ url('/download/' . $item->id) }}"><i class="bi bi-download"></i></a>
-                <button type="button" class="inline-flex items-center transition-colors font-medium select-none disabled:opacity-50 disabled:cursor-not-allowed focus:outline-none focus:ring focus:ring-offset-2 focus:ring-offset-white dark:focus:ring-offset-dark-eval-2 px-4 py-2 text-base bg-green-500 text-white hover:bg-green-600 focus:ring-green-500 rounded-md">
+                <button type="button" class="inline-flex items-center transition-colors font-medium select-none disabled:opacity-50 disabled:cursor-not-allowed focus:outline-none focus:ring focus:ring-offset-2 focus:ring-offset-white dark:focus:ring-offset-dark-eval-2 px-4 py-2 text-base bg-gray-400 text-white hover:bg-gray-600 focus:ring-gray-500 rounded-md">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
                 <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"></path>
                 <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"></path>
@@ -122,28 +122,28 @@
                         <div class="mt-4">
                         <ul class="list-group">
                             <li class="list-group-item d-flex justify-content-between align-items-center fw-bold">
-                            INTROVERT (I) <span  class="badge bg-secondary rounded-pill">{{round ($item->average_score_i*100) }} %</span> </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center fw-bold"> EKSTROVERT (E) <span  class="badge bg-secondary rounded-pill">{{round ($item->average_score_e*100) }} %</span> </li>
+                            INTROVERT (I) <span  class="badge bg-dark rounded-pill">{{round ($item->average_score_i*100) }} %</span> </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center fw-bold"> EKSTROVERT (E) <span  class="badge bg-dark rounded-pill">{{round ($item->average_score_e*100) }} %</span> </li>
                         </ul>
                         <div class="mt-4">
                         <ul class="list-group">
                             <li class="list-group-item d-flex justify-content-between align-items-center fw-bold">
-                            SENSING (S) <span  class="badge bg-secondary rounded-pill">{{round ($item->average_score_s*100) }} %</span> </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center fw-bold">  INTUITION (N) <span  class="badge bg-secondary rounded-pill">{{round ($item->average_score_n*100) }} %</span> </li>
-                        </ul>
-                        </div>
-                        <div class="mt-4">
-                        <ul class="list-group">
-                            <li class="list-group-item d-flex justify-content-between align-items-center fw-bold">
-                            THINKING (T) <span  class="badge bg-secondary rounded-pill">{{round ($item->average_score_t*100) }} %</span> </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center fw-bold">  FEELING (F) <span  class="badge bg-secondary rounded-pill">{{round ($item->average_score_f*100) }} %</span> </li>
+                            SENSING (S) <span  class="badge bg-dark rounded-pill">{{round ($item->average_score_s*100) }} %</span> </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center fw-bold">  INTUITION (N) <span  class="badge bg-dark rounded-pill">{{round ($item->average_score_n*100) }} %</span> </li>
                         </ul>
                         </div>
                         <div class="mt-4">
                         <ul class="list-group">
                             <li class="list-group-item d-flex justify-content-between align-items-center fw-bold">
-                            JUDGING (J) <span  class="badge bg-secondary rounded-pill">{{round ($item->average_score_j*100) }} %</span> </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center fw-bold">  PERCEIVING (P) <span  class="badge bg-secondary rounded-pill">{{round ($item->average_score_p*100) }} %</span> </li>
+                            THINKING (T) <span  class="badge bg-dark rounded-pill">{{round ($item->average_score_t*100) }} %</span> </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center fw-bold">  FEELING (F) <span  class="badge bg-dark rounded-pill">{{round ($item->average_score_f*100) }} %</span> </li>
+                        </ul>
+                        </div>
+                        <div class="mt-4">
+                        <ul class="list-group">
+                            <li class="list-group-item d-flex justify-content-between align-items-center fw-bold">
+                            JUDGING (J) <span  class="badge bg-dark rounded-pill">{{round ($item->average_score_j*100) }} %</span> </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center fw-bold">  PERCEIVING (P) <span  class="badge bg-dark rounded-pill">{{round ($item->average_score_p*100) }} %</span> </li>
                         </ul>
                         </div>
                         <div class="mt-3">
